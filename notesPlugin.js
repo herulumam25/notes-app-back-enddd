f747da9ef2470806abdab9660d921032621d2433
chairul umam <umamherul77@gmail.com>
2023-05-03 17:07:07 +0800

    initial commit

---
+++ notesPlugin.js
@@ -0,0 +1,19 @@
+const { server } = require("@hapi/hapi");
+const { options } = require("@hapi/hapi/lib/cors");
+
+module.exports = {
+    name: 'notes',
+    version : '1.0.0',
+    register:async (server,options) => {
+        const notes = options.notes;
+        server.route([
+            {
+                method: 'GET',
+                path: '/notes',
+                handler: () => {
+                    return notes
+                }
+            }
+        ])
+    },
+}
---
+++ src/api/notes/routes.js
@@ -0,0 +1,33 @@
+const { handler } = require("@hapi/hapi/lib/cors");
+const { getAllNotesHandler } = require("../../handler");
+
+const routes = (handler) => [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: handler.postNoteHandler,
+      },
+      {
+        method: 'GET',
+        path: '/notes',
+        handler: handler.getNotesHandler,
+      },
+      {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: handler.getNoteByIdHandler,
+      },
+      {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: handler.putNoteByIdHandler,
+      },
+      {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: handler.deleteNoteByIdHandler,
+      },
+
+];
+
+module.exports = routes;
